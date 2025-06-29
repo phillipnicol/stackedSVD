cc6d4adc90c7b9506b052d8477fc2bd0636f1356
phillipnicol <43178367+phillipnicol@users.noreply.github.com>
2025-06-28 21:53:55 -0400

    Auto-commit from makefile

---
+++ analysis/r_supp_plots/code/corollary_1.R
@@ -0,0 +1,43 @@
+setwd(here::here("analysis/r_supp_plots/code"))
+
+M <- 3
+
+c0 <- 1
+
+theta0 <- seq(0.5, 1.5, by=0.0005)
+
+#stacksvd <- ifelse(theta0 > M^{-1/2}*c0^{1/4},
+#                   1 - (c0 + M*theta0^2)/(M*theta0^4 + theta0^2),
+#                   0)
+
+stacksvd <- ifelse(theta0 > M^{-1/4}*c0^{1/4},
+                   1 - (c0 + theta0^2)/(M*theta0^4 + theta0^2),
+                   0)
+
+svdstack <- ifelse(theta0 > c0^{1/4},
+                   1 - (c0 + theta0^2)/(M*theta0^4 + theta0^2 - (M-1)*c0),
+                   0)
+
+df <- data.frame(x=theta0,
+                 stacksvd = stacksvd,
+                 svdstack = svdstack)
+
+library(reshape2)
+
+df <- melt(df, id.vars = "x")
+
+library(ggplot2)
+
+p <- ggplot(data=df,aes(x=x,y=value, color=variable)) +
+  geom_line() +
+  theme_bw() +
+  geom_vline(xintercept=3^{-1/4}, linetype="dashed", color="grey") +
+  geom_vline(xintercept=1, linetype="dashed", color="grey") +
+  xlab("theta0") +
+  ylab("Squared inner product") +
+  labs(color="Method")
+
+ggsave(p, filename="../plots/corollary_1_plot.png")
+
+
+
